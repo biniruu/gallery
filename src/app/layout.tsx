99940e8fa3016072d40c652f672ed6b5006bdde8
biniruu <biniruu@gmail.com>
2023-10-05 18:03:08 +0900

    feat: add footer component to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ReactNode } from 'react'
 
+import Footer from 'components/Footer'
 import Header from 'components/Header'
 import './globals.css'
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={inter.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
