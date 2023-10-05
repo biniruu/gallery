89d40c4bd0cde4cdefb7c48315a9382ff9b42dfa
biniruu <biniruu@gmail.com>
2023-10-05 18:01:22 +0900

    feat: create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="flex justify-between">
+      {/* TODO: Add copyright */}
+      <p>copyright</p>
+      {/* TODO: Add social media icons as shortcuts */}
+      <ul>
+        <li>instagram</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer
